d69c226cb90116e05456df3c4a09932927ea1410
Daniel Díaz <dandia28@gmail.com>
2020-05-04 20:22:48 -0300

    Agregar archivos de proyecto.

---
+++ frmMain/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMatematicas = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtHistoria = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtProgramacion = New System.Windows.Forms.TextBox()
+        Me.emdPromedio = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(48, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre del Alumno:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(61, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 1
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(51, 25)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(159, 20)
+        Me.txtNombre.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(48, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Calificación Matematicas:"
+        '
+        'txtMatematicas
+        '
+        Me.txtMatematicas.Location = New System.Drawing.Point(51, 84)
+        Me.txtMatematicas.Name = "txtMatematicas"
+        Me.txtMatematicas.Size = New System.Drawing.Size(26, 20)
+        Me.txtMatematicas.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(48, 119)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(99, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Calificación Historia"
+        '
+        'txtHistoria
+        '
+        Me.txtHistoria.Location = New System.Drawing.Point(51, 145)
+        Me.txtHistoria.Name = "txtHistoria"
+        Me.txtHistoria.Size = New System.Drawing.Size(26, 20)
+        Me.txtHistoria.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(48, 178)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(129, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Calificación Programación"
+        '
+        'txtProgramacion
+        '
+        Me.txtProgramacion.Location = New System.Drawing.Point(51, 204)
+        Me.txtProgramacion.Name = "txtProgramacion"
+        Me.txtProgramacion.Size = New System.Drawing.Size(26, 20)
+        Me.txtProgramacion.TabIndex = 8
+        '
+        'emdPromedio
+        '
+        Me.emdPromedio.BackColor = System.Drawing.SystemColors.ControlText
+        Me.emdPromedio.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.emdPromedio.Location = New System.Drawing.Point(160, 215)
+        Me.emdPromedio.Name = "emdPromedio"
+        Me.emdPromedio.Size = New System.Drawing.Size(120, 46)
+        Me.emdPromedio.TabIndex = 9
+        Me.emdPromedio.Text = "CALCULAR PROMEDIO"
+        Me.emdPromedio.UseVisualStyleBackColor = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(292, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.Controls.Add(Me.emdPromedio)
+        Me.Controls.Add(Me.txtProgramacion)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtHistoria)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtMatematicas)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Bienvenido"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtMatematicas As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtHistoria As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtProgramacion As System.Windows.Forms.TextBox
+    Friend WithEvents emdPromedio As System.Windows.Forms.Button
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+
+End Class
---
+++ frmMain/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Function Promedio(ByVal notaMat As Integer, ByVal notaHis As Integer, ByVal notaPro As Integer) As Integer
+
+        Return (notaMat + notaHis + notaPro) / 3
+
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles emdPromedio.Click
+        Dim Prom As Integer
+        Prom = Promedio(CInt(txtHistoria.Text), CInt(txtMatematicas.Text), CInt(txtProgramacion.Text))
+        If Prom <= 7 Then
+            MsgBox("PROMEDIO: " & txtNombre.Text & " " & Prom, MsgBoxStyle.Critical)
+        ElseIf Prom <= 9 Then
+            MsgBox("PROMEDIO: " & txtNombre.Text & " " & Prom, MsgBoxStyle.Exclamation)
+        ElseIf Prom <= 12 Then
+            MsgBox("PROMEDIO: " & txtNombre.Text & " " & Prom, MsgBoxStyle.Information)
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.TextChanged
+
+
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtHistoria.TextChanged
+
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
